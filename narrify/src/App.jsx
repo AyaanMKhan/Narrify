d25d442e3133bf1cff6bc06426df338657f00ace
Ayaan Khan <ayaanmk2004@gmail.com>
2025-07-22 00:55:51 -0400

    set up correctly

---
+++ narrify/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App(){
+  return (
+    <div>
+      <h1>Narrify</h1>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
+  );
+}
+
+
+export default App;
